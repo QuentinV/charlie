0a73aae2cb83959208ad2e677183273857e85ccf
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-02-17 01:02:04 +0100

    deploy frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const apihost = process?.env?.API_HOST ?? 'http://localhost:9300';
+console.log(apihost);
+
 // https://vite.dev/config/
 export default defineConfig({
+    root: '',
+    base: '/',
     plugins: [react()],
     server: {
+        host: true,
+        port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:9300',
+                target: apihost,
                 changeOrigin: true,
             },
         },
