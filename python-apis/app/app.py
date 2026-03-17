6bc3fa4f76da0ac52a85a1f729c0fa968fc4063f
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-03-17 22:15:41 +0100

    llm

--- python-apis/app/app.py
+++ python-apis/app/app.py
@@ -9,15 +9,21 @@ import json
 import requests
 import sys
 
-from routers import stt_vosk, tts, llm, stt_qwen
+from routers import stt_vosk, tts, llm, stt_qwen, llm_qwen
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
 
 sttModel = os.getenv("STT_MODEL")
+llmModel = os.getenv("LLM_MODEL")
 
-app.include_router(llm.router)
-app.include_router(tts.router)
+print(f"Running with LLM_MODEL = {llmModel} and STT_MODEL = {sttModel}")
+
+if llmModel == "mistreal":
+    app.include_router(llm.router)
+elif llmModel == "qwen":
+    llm_qwen.load_model()
+    app.include_router(llm_qwen.router)
 
 if sttModel == "vosk":
     stt_vosk.load_model()
@@ -26,6 +32,7 @@ elif sttModel == "qwen":
     stt_qwen.load_model()
     app.include_router(stt_qwen.router)
 
+app.include_router(tts.router)
 
 @app.get("/")
 async def root():
