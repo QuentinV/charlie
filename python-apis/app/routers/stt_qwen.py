00ac85ffdcd611f689569b080b8b685f2eb0ce7a
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-03-20 20:12:33 +0100

    lib

--- python-apis/app/routers/stt_qwen.py
+++ python-apis/app/routers/stt_qwen.py
@@ -37,10 +37,11 @@ def load_model():
     model = Qwen3ASRModel.from_pretrained(
         LOCAL_MODEL_ASR_DIR,
         device_map="cpu",
-        #dtype=torch.float32 
-        dtype=torch.bfloat16, 
+        dtype=torch.float32, 
+        #dtype=torch.bfloat16, 
         low_cpu_mem_usage=True,
-        max_new_tokens = 128
+        max_new_tokens = 128,
+        attn_implementation="sdpa" # Scaled Dot Product Attention
     )
 
     print("🚀 Warming up engine...")
