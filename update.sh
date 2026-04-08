94ce04e8a5599f06377eaff1e483dde0a475171b
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-04-08 23:55:27 +0200

    update

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ docker compose stop
 docker compose up api --build -d
 docker compose up frontend --build -d
 docker compose up ai-agents --build -d
-docker compose up --remove-orphans -d
+docker compose --profile cpu up --remove-orphans -d
