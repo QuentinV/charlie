12ef0ce6fb6b241f87616882dce16d3d68221278
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-04-12 22:55:13 +0200

    network scanner api

--- network-scanner/src/index.js
+++ network-scanner/src/index.js
@@ -1,4 +1,5 @@
 import { exec } from 'child_process';
+import express from 'express';
 
 export async function getNetworkDevices(subnet) {
     const refreshCommand = `for i in {1..255}; do ping -c 1 -W 1 ${subnet}$i > /dev/null 2>&1 & done; wait`;
@@ -32,107 +33,31 @@ export async function getNetworkDevices(subnet) {
                     }
                 }
             }
-            resolve(devices);
+            resolve(Object.entries(devices).map(([mac, ip]) => ({ mac, ip })));
         });
     });
 }
 
-import 'dotenv/config';
-import { initAll } from './init';
-import express from 'express';
-import fs from 'fs';
-import https from 'https';
-import cors from 'cors';
-import managerApis from './manager';
-import { getProvidersRestApis } from './devices';
-import { buildMcpServer } from './tools/mcp/sse_server';
-import { HttpError } from './errors';
-import swaggerUi from 'swagger-ui-express';
-import { defaultApiPath, getSwaggerDoc } from './swagger';
-import { registerNotificationApi } from './core/notifications';
-import { setupEchoListen } from './echo/listen';
-import { setupMqttServer } from './messaging/receive';
-import { setupRoutines } from './routines';
-import { setupRotateProvidersIp } from './devices/rotateip.job';
-
 const app = express();
+app.use(express.json());
 
-(async () => {
-    await initAll();
+app.get('/api/devices', async (req, res) => {
+    console.log('/api/devices');
+    const s = req.query?.subnet ?? '192.168.1';
+    const subnet = s + '.';
+    res.send(await getNetworkDevices(subnet));
+});
 
-    const swaggerDoc = await getSwaggerDoc();
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
 
-    app.use(express.json());
-    app.use(
-        cors({
-            origin: '*',
-            exposedHeaders: ['Mcp-Session-Id'],
-            allowedHeaders: ['Content-Type', 'mcp-session-id'],
-        })
-    );
+app.get('/ping', (req, res) => res.send('pong'));
 
-    // init endpoints
-    const apis = { ...managerApis, ...(await getProvidersRestApis()) };
-    Object.entries(apis).forEach(([path, fullapi]) => {
-        const builtPath = `${defaultApiPath}${path}`;
-        Object.entries(fullapi).forEach(([methodName, endpoint]) => {
-            (app as any)[methodName](builtPath, async (req: any, res: any) => {
-                try {
-                    if (endpoint?.fullHandler) {
-                        await endpoint.fullHandler(req, res);
-                    } else if (endpoint?.handler || endpoint) {
-                        const result = await (endpoint?.handler ?? endpoint)(
-                            req
-                        );
-                        result ? res.send(result) : res.sendStatus(204);
-                    }
-                } catch (e) {
-                    if (e instanceof HttpError) {
-                        res.send(e.httpStatus, e.message);
-                        return;
-                    }
-                    res.sendStatus(500);
-                }
-            });
-            console.log(`[API] ${methodName.toUpperCase()}`, builtPath);
-        });
-    });
-
-    registerNotificationApi(app);
-
-    // Error handler
-    app.use((err: any, req: any, res: any, next: any) => {
-        console.error(err.stack);
-        res.status(500).json({ message: 'Something went wrong!' });
-    });
-
-    await buildMcpServer(app);
-
-    // Start the Express server
-    const port = 9300;
-    let hserver = null;
-    try {
-        const privateKey = fs.readFileSync(
-            '../invData/invDataService/certs/live/mymovies.freeboxos.fr/privkey.pem'
-        );
-        const certificate = fs.readFileSync(
-            '../invData/invDataService/certs/live/mymovies.freeboxos.fr/fullchain.pem'
-        );
-
-        hserver = https.createServer(
-            { key: privateKey, cert: certificate },
-            app
-        );
-        hserver.listen(port, () => {
-            console.log(`Https server listening on port ${port}`);
-        });
-    } catch (e) {
-        hserver = app.listen(port, () => {
-            console.log(`Http server listening on port ${port}`);
-            console.log(
-                `Swagger available here: http://localhost:${port}/api-docs`
-            );
-        });
-    }
-})();
+// Start the Express server
+const port = 9306;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Http server listening on port ${port}`);
+});
