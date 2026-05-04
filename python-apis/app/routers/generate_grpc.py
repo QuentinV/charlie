708c200e8b9fb77d84c3e9f2359bca2f618c7157
Quentin Vanhauteghem <quentvht@hotmail.com>
2026-05-05 00:48:00 +0200

    chat grpc

--- python-apis/app/routers/generate_grpc.py
+++ python-apis/app/routers/generate_grpc.py
@@ -2,19 +2,29 @@ import subprocess
 import sys
 import os
 
-PROTO_PATH = "/mcp-server/src"
-PROTO_FILE = "grpc_api.proto"
-
 def build_protos():
     result = subprocess.run([
         sys.executable, "-m", "grpc_tools.protoc",
-        f"-I{PROTO_PATH}",
+        f"-I/mcp-server/src",
         "--python_out=.",
         "--grpc_python_out=.",
-        os.path.join(PROTO_PATH, PROTO_FILE)
+        "/mcp-server/src/grpc_api.proto"
     ], capture_output=True, text=True)
 
     if result.returncode != 0:
         print(f"Error: {result.stderr}")
     else:
-        print("Successfully generated gRPC stubs.")
+        print("Successfully generated gRPC MCP tools stubs.")
+
+    result = subprocess.run([
+        sys.executable, "-m", "grpc_tools.protoc",
+        f"-I.",
+        "--python_out=.",
+        "--grpc_python_out=.",
+        "chat.proto"
+    ], capture_output=True, text=True)
+
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+    else:
+        print("Successfully generated gRPC AI Chat stubs.")
